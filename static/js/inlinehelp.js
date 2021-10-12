94f6cab1ba869c1332a21116dbeed0bd44b00b98
sunnavy <sunnavy@bestpractical.com>
2021-10-13 01:47:55 +0800

    Convert tabs to whitespaces

--- static/js/inlinehelp.js
+++ static/js/inlinehelp.js
@@ -1,121 +1,121 @@
 // a list of entries to process for the page
 var pagePopupHelpItems = [
-	{ selector: "[data-help]", action: helpify }  // by default, anything with data-help attributes gets processed
+    { selector: "[data-help]", action: helpify }  // by default, anything with data-help attributes gets processed
 ];
 
 // add one or more items to the list of help entries to process for the page
 function addPopupHelpItems() {
-	const args = [].slice.call(arguments).reduce(function(acc,val) { return acc.concat(val) }, [] );
-	pagePopupHelpItems = pagePopupHelpItems || [];
-	pagePopupHelpItems = pagePopupHelpItems.concat(args);
+    const args = [].slice.call(arguments).reduce(function(acc,val) { return acc.concat(val) }, [] );
+    pagePopupHelpItems = pagePopupHelpItems || [];
+    pagePopupHelpItems = pagePopupHelpItems.concat(args);
 }
 
 function applySelectorQueryOrFunc( sel ) {
-	if ( sel ) {
-		if ( typeof(sel) === "string" ) {
-			return jQuery(sel);
-		} else if ( typeof(sel) === "function" ) {
-			return sel(jQuery);
-		}
-	}
+    if ( sel ) {
+        if ( typeof(sel) === "string" ) {
+            return jQuery(sel);
+        } else if ( typeof(sel) === "function" ) {
+            return sel(jQuery);
+        }
+    }
 }
 
 function getPopupHelpAction( entry={} ) {
-	entry.action = entry.action || "append"
-	if ( typeof(entry.action) === "string" ) {
-		const funcMap = {
-			"before": beforePopupHelp,
-			"after": afterPopupHelp,
-			"append": appendPopupHelp,
-			"prepend": prependPopupHelp,
-			"offset": offsetPopupHelp,
-			"replace": replacePopupHelp
-		}
-		if (funcMap.hasOwnProperty(entry.action)) {
-			return funcMap[entry.action];
-		} else {
-			console.error("Unknown action '" + entry.action + "' using 'after' instead");
-			return funcMap.after;
-		}
-	} else if ( typeof(entry.action) === "function" ) {
-		return entry.action;
-	}
+    entry.action = entry.action || "append"
+    if ( typeof(entry.action) === "string" ) {
+        const funcMap = {
+            "before": beforePopupHelp,
+            "after": afterPopupHelp,
+            "append": appendPopupHelp,
+            "prepend": prependPopupHelp,
+            "offset": offsetPopupHelp,
+            "replace": replacePopupHelp
+        }
+        if (funcMap.hasOwnProperty(entry.action)) {
+            return funcMap[entry.action];
+        } else {
+            console.error("Unknown action '" + entry.action + "' using 'after' instead");
+            return funcMap.after;
+        }
+    } else if ( typeof(entry.action) === "function" ) {
+        return entry.action;
+    }
 }
 
 function getPopupHelpActionArgs( entry={}, $els ) {
-	return entry.actionArgs ? [ $els, entry, entry.actionArgs ] : [ $els, entry ];
+    return entry.actionArgs ? [ $els, entry, entry.actionArgs ] : [ $els, entry ];
 }
 
 function beforePopupHelp( $els, item={}, options={} ) {
-	item.action = options.action = "before";
-	return helpify( $els, item, options );
+    item.action = options.action = "before";
+    return helpify( $els, item, options );
 }
 
 function afterPopupHelp( $els, item={}, options={} ) {
-	item.action = options.action = "after";
-	return helpify( $els, item, options );
+    item.action = options.action = "after";
+    return helpify( $els, item, options );
 }
 
 function appendPopupHelp( $els, item={}, options={} ) {
-	item.action = options.action = "append";
-	return helpify( $els, item, options );
+    item.action = options.action = "append";
+    return helpify( $els, item, options );
 }
 
 function prependPopupHelp( $els, item={}, options={} ) {
-	item.action = options.action = "prepend";
-	return helpify( $els, item, options );
+    item.action = options.action = "prepend";
+    return helpify( $els, item, options );
 }
 
 function offsetPopupHelp( $els, item={}, options={} ) {
-	item.action = options.action = "offset";
-	return helpify( $els, item, options );
+    item.action = options.action = "offset";
+    return helpify( $els, item, options );
 }
 
 function replacePopupHelp( $els, item={}, options={} ) {
-	item.action = options.action = "replace";
-	return helpify( $els, item, options );
+    item.action = options.action = "replace";
+    return helpify( $els, item, options );
 }
 
 function helpify($els, item={}, options={}) {
-	$els.each(function(index) {
-		const $el = jQuery($els.get(index));
-		const action = $el.data("action") || item.action || options.action;
-		const title = $el.data("title") || item.title || $el.data("help");
-		const content = $el.data("content") || item.content;
-		switch(action) {
-			case "before":
-				$el.before( buildPopupHelpHtml( title, content ) );
-				break;
-			case "prepend":
-				$el.prepend( buildPopupHelpHtml( title, content ) );
-				break;
-			case "offset":
-				$el.append( buildPopupHelpHtml( title, content ) ).offset( options );
-				break;
-			case "replace":
-				$el.replaceWith( buildPopupHelpHtml( title, content ) );
-				break;
-			case "append":
-				$el.append( buildPopupHelpHtml( title, content ) );
-				break;
-			case "after":  // intentionally fallthrough, as 'after' is the default
-			default:
-				$el.parent().append( buildPopupHelpHtml( title, content ) );
-		}
-	})
+    $els.each(function(index) {
+        const $el = jQuery($els.get(index));
+        const action = $el.data("action") || item.action || options.action;
+        const title = $el.data("title") || item.title || $el.data("help");
+        const content = $el.data("content") || item.content;
+        switch(action) {
+            case "before":
+                $el.before( buildPopupHelpHtml( title, content ) );
+                break;
+            case "prepend":
+                $el.prepend( buildPopupHelpHtml( title, content ) );
+                break;
+            case "offset":
+                $el.append( buildPopupHelpHtml( title, content ) ).offset( options );
+                break;
+            case "replace":
+                $el.replaceWith( buildPopupHelpHtml( title, content ) );
+                break;
+            case "append":
+                $el.append( buildPopupHelpHtml( title, content ) );
+                break;
+            case "after":  // intentionally fallthrough, as 'after' is the default
+            default:
+                $el.parent().append( buildPopupHelpHtml( title, content ) );
+        }
+    })
 }
 
 function buildPopupHelpHtml(title, content) {
-	const contentAttr = content ? ' data-content="' + content + '" ' : '';
-	return '<a class="popup-help" tabindex="0" role="button" data-toggle="popover" title="' + title + '" data-trigger="hover" ' + contentAttr + '><span class="far fa-question-circle fa-lg"></span></a>';
+    const contentAttr = content ? ' data-content="' + content + '" ' : '';
+    return '<a class="popup-help" tabindex="0" role="button" data-toggle="popover" title="' + title + '" data-trigger="hover" ' + contentAttr + '><span class="far fa-question-circle fa-lg"></span></a>';
 }
 
 function applyPopupHelpAction( entry, $els ) {
-	if ( entry ) {
-		const fn = getPopupHelpAction( entry );
-		const args = getPopupHelpActionArgs( entry, $els );
-		fn.apply(this, args);
-	}
+    if ( entry ) {
+        const fn = getPopupHelpAction( entry );
+        const args = getPopupHelpActionArgs( entry, $els );
+        fn.apply(this, args);
+    }
 }
 
 // Dynamically load the help topic corresponding to a DOM element using AJAX
